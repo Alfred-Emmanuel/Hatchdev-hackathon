f22845f50a6c13eea1c999d985f12d64f22c60f0
Alfred <ealfred828@gmail.com>
2024-08-31 13:19:45 +0100

    set up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+import Products from './pages/Products';
+import Brands from './pages/Brands';
 
 function App() {
   
-
   return (
-    <>
-       <h1 className="bg-black text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/brands" element={<Brands />} />
+        <Route path="/product_list" element={<ProductList />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </Router>
+  );
+
 }
 
 export default App
