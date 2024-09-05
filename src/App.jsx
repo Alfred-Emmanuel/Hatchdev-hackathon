a1f86f75395871878a7cc959a8bed2803bc73370
Alfred <ealfred828@gmail.com>
2024-09-05 13:26:29 +0100

    Created an about page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Products from './pages/Products';
 import Brands from './pages/Brands';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import About from './pages/About';
 
 function App() {
   
@@ -18,6 +19,7 @@ function App() {
         <Route path="/products" element={<Products />} />
         <Route path="/sign_in" element={<SignIn />} />
         <Route path="/register" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
