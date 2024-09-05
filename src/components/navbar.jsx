1e0c83ac324d43ed9f0c7995e3ed9b747d52d5f8
Alfred <ealfred828@gmail.com>
2024-09-05 15:36:45 +0100

    trying to solve a routing issue in prod

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import { FaChevronDown } from "react-icons/fa";
 import { BsSearch, BsCart, BsList } from 'react-icons/bs';
+import useAuthDetails from '../custom-hooks/useAuthDetails';
 
 const Navbar = () => {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const { authUser, userSignOut } = useAuthDetails();
+    console.log (authUser)
 
     return (
         <nav className="mb-11 w-full absolute top-0 left-0 text-white py-4 bg-transparent">
@@ -15,14 +18,8 @@ const Navbar = () => {
                 <div className="flex flex-col lg:flex-row lg:space-x-8 lg:items-center w-full lg:w-auto text-blue-950">
                     <div className={`flex flex-col lg:flex-row lg:space-x-8 lg:items-center ${isMobileMenuOpen ? 'block' : 'hidden lg:flex'}`}>
                         <a href="#" className="hover:text-gray-400">Home</a>
-                        <div className="relative flex items-center">
-                            <span>Shop</span>
-                            <button
-                                onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-                                className="ml-1 focus:outline-none"
-                            >
-                                <FaChevronDown className="w-5 h-5" />
-                            </button>
+                        <div onClick={() => setIsDropdownOpen(!isDropdownOpen)} className="relative flex items-center">
+                            <span>Brands</span>
                             {isDropdownOpen && (
                                 <div className="absolute bg-gray-800 p-2 mt-2 rounded shadow-lg">
                                     <a href="#" className="block px-4 py-2 hover:bg-gray-700 rounded">Option 1</a>
@@ -43,18 +40,19 @@ const Navbar = () => {
 
                 {/* Right Side - Request Demo and Icons */}
                 <div className={`flex items-center space-x-4 ${isMobileMenuOpen ? 'hidden' : 'lg:flex'}`}>
-                    <a
-                        href="/register"
-                        className="text-black px-4 py-1 w-[154px] h-[42px] bg-white rounded"
-                    >
-                        Login/ Register
-                    </a>
-                    <button className="text-xl">
-                        <BsSearch />
-                    </button>
-                    <button className="text-xl">
-                        <BsCart />
-                    </button>
+                    {
+                        authUser ? (
+                            <button className='text-black' onClick={userSignOut}>Logout</button>
+                        ) : 
+                        (
+                            <a
+                                href="/register"
+                                className="text-black px-4 py-1 w-[154px] h-[42px] bg-white rounded"
+                            >
+                                Login/ Register
+                            </a>
+                        )
+                    }
                 </div>
                  {/* Mobile Menu Button */}
                  <button
